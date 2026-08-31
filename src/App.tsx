bdcbc2df329f7564848460a3580e627525c291b7
Aravindh D <133035906+Aravindhdit@users.noreply.github.com>
2026-08-31 18:00:54 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,17 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import { useTheme } from "./hooks/useTheme";
 import Home from "./pages/Home";
 import CredentialVerification from "./pages/CredentialVerification";
 
 export default function App() {
-  // Initialise theme at the app root so the dark class is applied immediately
   useTheme();
 
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/credentials" element={<CredentialVerification />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
